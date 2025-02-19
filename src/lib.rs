fec393e6936a04dd098491fb3c97050064eb202a
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2025-02-19 11:30:24 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#[cfg(feature = "automation")]
+pub mod automation;
+
+#[cfg(feature = "audio")]
+pub mod audio;
+
+/*
+use crate::path::Path;
+use crate::env::get_env;
+use crate::sjis::*;
+use crate::binaryfile::{BinaryReader, BinaryWriter};
+use crate::csvreader::CSVReader;
+use crate::textreader::TextReader;
+use crate::clip::*;
+use crate::toml::read;
+use crate::audio::Audio;
+use crate::compress::compress;
+*/
